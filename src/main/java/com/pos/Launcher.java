62bbff7c62150bb4652d7aab2e1226e84bf4941a
Jie Sheng <jasonng0315@gmail.com>
2026-06-16 16:57:16 +0800

    added debugger

---
+++ src/main/java/com/pos/Launcher.java
@@ -0,0 +1,8 @@
+package com.pos;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // This quietly triggers your actual JavaFX application
+        App.main(args); 
+    }
+}
